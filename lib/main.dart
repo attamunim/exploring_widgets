838df186dd1edd07f9fe0785974e7b01bd260019
attamunim <ulmunim@gmail.com>
2022-08-01 22:17:12 +0500

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -18,26 +18,69 @@ class MyApp extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Stack(children: [
-              Container(
-                height: 180,
-                width: 180,
-                color: Colors.red,
-              ),
-              Container(
-                height: 140,
-                width: 140,
-                color: Colors.green,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue,
-              ),
-            ])
+            SizedBox(
+                child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 180,
+                  width: 180,
+                  color: Colors.red,
+                ),
+                Text('Atta ul Munim'),
+              ],
+            )),
+            Stack(
+              children: [
+                Container(
+                  height: 180,
+                  width: 180,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 150,
+                  width: 150,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
+
+//  Expanded(
+//               flex: 2,
+//               child: Container(
+//                 height: 250,
+//                 color: Colors.green,
+//                 child: const Center(child: Text('Hello')),
+//               ),
+//             ),
+//             Center(
+//               child: Container(
+//                 decoration: BoxDecoration( 
+//                   color: Colors.orange,
+//                   borderRadius: const BorderRadius.only(
+//                     topRight: const Radius.circular(16),
+//                   ),
+//                   border: Border.all(color: Colors.red, width: 3),
+//                   boxShadow: [
+//                     const BoxShadow(
+//                       color: Colors.grey,
+//                       blurRadius: 100,
+//                     ),
+//                   ],
+//                 ),
+//                 child: const Center(
+//                   child: const Text('container 1'),
+//                 ),
+//               ),
+//             ),
